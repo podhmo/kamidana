c9d74f43db4772714066bacdd7dcbde445129f86
podhmo <ababjam61+github@gmail.com>
2017-09-25 23:48:59 +0900

    rename (rebase)

--- kamidana/commands/onefile.py
+++ kamidana/commands/onefile.py
@@ -1,10 +1,12 @@
-import argparse
+import logging
 from magicalimport import import_symbol
+from dictknife import loading
+from dictknife.langhelpers import traceback_shortly
 
 
 def main():
+    import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data", action="append", help="support yaml, json, toml", default=[])
     parser.add_argument(
         "--driver",
         default="kamidana.driver:Driver",
@@ -15,21 +17,31 @@ def main():
         default="kamidana.loader:TemplateLoader",
         help="default: kamidana.loader:TemplateLoader",
     )
-    parser.add_argument("--additionals", default=None)
+    parser.add_argument(
+        "-d", "--data", action="append", help="support yaml, json, toml", default=[]
+    )
+    parser.add_argument("--logging", choices=list(logging._nameToLevel.keys()), default="INFO")
+    parser.add_argument("-a", "--additionals", action="append", default=[])
+    parser.add_argument("-e", "--extension", action="append", default=[])
     parser.add_argument("-i", "--input-format", default=None)
     parser.add_argument("-o", "--output-format", default="raw")
     parser.add_argument("--dump-context", action="store_true")
     parser.add_argument("template")
+    parser.add_argument("--debug", action="store_true")
     parser.add_argument("--dst", default=None)
 
     args = parser.parse_args()
+    logging.basicConfig(level=getattr(logging, args.logging))
+    with traceback_shortly(args.debug):
+        loader_cls = import_symbol(args.loader, ns="kamidana.loader")
+        extensions = [
+            ("jinja2.ext.{}".format(ext) if "." not in ext else ext) for ext in args.extension
+        ]
+        loader = loader_cls(args.data, args.additionals, extensions, format=args.input_format)
 
-    loader_cls = import_symbol(args.loader, ns="kamidana.loader")
-    loader = loader_cls(args.data, args.additionals, args.input_format)
-
-    if args.dump_context:
-        driver_cls = import_symbol("ContextDumpDriver", ns="kamidana.driver")
-    else:
-        driver_cls = import_symbol(args.driver, ns="kamidana.driver")
-    driver = driver_cls(loader, format=args.output_format)
-    driver.run(args.template, args.dst)
+        if args.dump_context:
+            driver_cls = import_symbol("ContextDumpDriver", ns="kamidana.driver")
+        else:
+            driver_cls = import_symbol(args.driver, ns="kamidana.driver")
+        driver = driver_cls(loader, format=args.output_format)
+        driver.run(args.template, args.dst)
