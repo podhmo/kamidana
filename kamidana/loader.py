40e32ec84e1fd2934d03937c339b36aef63a812a
podhmo <ababjam61+github@gmail.com>
2019-02-16 00:24:39 +0900

    refactor: drop loader cache, jinja2 has also tempalte cache, already

--- kamidana/loader.py
+++ kamidana/loader.py
@@ -17,18 +17,12 @@ class TemplateLoader(ITemplateLoader):
         self.additional_path_list = additional_path_list
         self.extensions = extensions
         self.format = format
-        self._cache = {}
 
     def load(self, filename):
-        data = self._cache.get(filename)
-        if data is not None:
-            return data
-
         try:
             with open(filename) as rf:
                 logger.debug("load: %s", filename)
-                data = self._cache[filename] = rf.read()
-            return data
+                return rf.read()
         except FileNotFoundError as e:
             raise XTemplatePathNotFound(filename, exc=e) from None
 
