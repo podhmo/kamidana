efa03096295fc85b801e6e98f6d5a9f667833dae
podhmo <ababjam61+github@gmail.com>
2019-02-16 20:45:39 +0900

    feat: nested traceback support

---
+++ kamidana/debug/_extract.py
@@ -0,0 +1,45 @@
+import traceback
+from collections import namedtuple
+
+FrameSet = namedtuple("FrameSet", "kind, frames")
+Detail = namedtuple("Detail", "jinja2, framesets, outermost")
+
+
+def extract_detail(exc: Exception, *, tb=None) -> Detail:
+    tb = tb or exc.__traceback__
+    aggs = _aggregate_traceback(tb)
+    if len(aggs) == 1:
+        return Detail(jinja2=[], framesets=aggs, outermost=False)
+
+    if aggs[-1].kind == "jinja2":
+        outermost = True
+        fsets = aggs[-1]
+    else:
+        outermost = False
+        fsets = aggs[-2]
+    f: traceback.FrameSummary = fsets[-1]
+    return Detail(jinja2=fsets, framesets=aggs, outermost=outermost)
+
+
+def _detect_kind(
+    name: str, *, _cands=set(["template", "top-level template code", "template"])
+) -> str:
+    is_jinja2 = name in _cands or name.startswith('block "')
+    return "jinja2" if is_jinja2 else "python"
+
+
+def _aggregate_traceback(tb, *, detect_kind=_detect_kind):
+    frames = traceback.extract_tb(tb)
+    kind = None
+    cur = []
+    r = []
+    for fs in frames:
+        prev, kind = kind, detect_kind(fs.name)
+        if prev != kind:
+            if cur:
+                r.append(FrameSet(kind=prev, frames=cur))
+            cur = []
+        cur.append(fs)
+    if cur:
+        r.append(FrameSet(kind=kind, frames=cur))
+    return r
--- kamidana/debug/error_handling.py
+++ kamidana/debug/error_handling.py
@@ -1,42 +1,45 @@
 import os
 import textwrap
 import traceback
+import linecache
+import logging
 from collections import defaultdict
 from io import StringIO
+
 import jinja2
+
 from .._path import XTemplatePathNotFound
 from .color import highlight
+from ._extract import extract_detail
 
-
-def _get_info_from_exception(exc: jinja2.TemplateError):
-    d = {
-        "exc_class": "{}.{}".format(exc.__module__, exc.__class__.__name__),
-        "message": str(exc),
-    }
-
-    if hasattr(exc, "filename"):
-        d["where"] = exc.filename or exc.name
-        return d
-
-    # hack: original expression from hidden api
-    if hasattr(exc, "original_context"):
-        octx = exc.original_context
-        if octx.where is not None:
-            d["where"] = os.path.relpath(octx.where, os.getcwd())
-        d["message"] = d["message"].replace(exc.args[0], octx.path)
-    return d
+logger = logging.getLogger(__name__)
 
 
 class GentleOutputRenderer:
     def __init__(
-        self, *, n: int, full: bool = False, formatter=None, colorful: bool = False
+        self,
+        *,
+        n: int,
+        full: bool = False,
+        formatter=None,
+        line_formatter=None,
+        colorful: bool = False
     ):
         self.n = n
         self.full = full  # todo: handling (currently, ignored)
-        self.formatter = formatter or self._format
         self.colorful = colorful
+        # todo: separation
+        self.formatter = formatter or self._format
+        self.line_formatter = line_formatter or self._line_format
 
-    def _format(self, d):
+    def _line_format(self, i: int, line: str, *, lineno: int) -> str:
+        size = len(str(lineno + self.n))
+        text = ("{lineno: %d}: {line}" % (size)).format(lineno=i, line=line)
+        if i == lineno:
+            return highlight("  -> {}".format(text), colorful=self.colorful)
+        return "     {}".format(text)
+
+    def _format(self, d) -> str:
         d = defaultdict(lambda: None, d)
         fmt = textwrap.dedent(
             """
@@ -66,15 +69,68 @@ class GentleOutputRenderer:
     def get_information(self, exc: Exception) -> str:
         if isinstance(exc, jinja2.TemplateSyntaxError):
             return self.on_syntax_error(exc)
-        elif isinstance(exc, (XTemplatePathNotFound, jinja2.TemplateError)):
+        # elif isinstance(exc, XTemplatePathNotFound):
+        #     return self.on_notfound_error(exc)
+        elif isinstance(exc, jinja2.TemplateError):
             return self.on_template_error(exc)
         else:
-            raise
+            return self.on_error(exc)
+
+    def on_notfound_error(self, exc: XTemplatePathNotFound) -> dict:
+        d = vars(exc).copy()
+        d.update(_get_info_from_exception(exc))
+        return d
 
     def on_template_error(self, exc: jinja2.TemplateError) -> dict:
         d = vars(exc).copy()
         d.update(_get_info_from_exception(exc))
         d["output"] = traceback.format_exc(limit=3)
+        return d
+
+    def on_error(self, exc: Exception, *, level: int = 3) -> dict:
+        # shape :: [python, jinja2, python, ....]
+        d = vars(exc).copy()
+        detail = extract_detail(exc)
+        if not detail.jinja2:
+            if isinstance(exc, XTemplatePathNotFound):
+                return self.on_notfound_error(exc)
+            raise exc.with_traceback(exc.__traceback__)
+
+        # todo: python
+        # todo: nested template
+        logger.debug(
+            "frame shape: %r", [(fs.kind, len(fs.frames)) for fs in detail.framesets]
+        )
+
+        buf = StringIO()
+        first = True
+        # jinja2's traceback
+        for i in reversed(range(1, min(level, len(detail.jinja2.frames) + 1))):
+            if first:
+                first = False
+            else:
+                print("", file=buf)
+
+            lineno = detail.jinja2.frames[-i].lineno
+            filename = detail.jinja2.frames[-i].filename
+            print("{}:".format(os.path.relpath(filename, os.getcwd())), file=buf)
+            start_lineno = max(1, lineno - self.n)
+            end_lineno = min(len(linecache.getlines(filename)) + 1, lineno + self.n + 1)
+            for i in range(start_lineno, end_lineno):
+                line = linecache.getline(filename, lineno=i).rstrip()
+                print(self.line_formatter(i, line, lineno=lineno), file=buf)
+
+        # python's traceback
+        if not detail.outermost:
+            frames = detail.framesets[-1].frames
+            lines = traceback.StackSummary.from_list(frames).format()
+            print("", file=buf)
+            print("Traceback:", file=buf)
+            print("".join(lines), file=buf)
+
+        d["where"] = os.path.relpath(filename, start=os.getcwd())  # xxx
+        d["output"] = buf.getvalue()
+        d.update(_get_info_from_exception(exc))
         return d
 
     def on_syntax_error(self, exc: jinja2.TemplateSyntaxError) -> dict:
@@ -93,16 +149,33 @@ class GentleOutputRenderer:
         for i, line in enumerate(
             lines[start_position:end_position], start_position + 1
         ):
-            size = len(str(lineno + self.n))
-            text = ("{lineno: %d}: {line}" % (size)).format(lineno=i, line=line)
-            if i == lineno:
-                print(highlight("-> {}".format(text), colorful=self.colorful), file=buf)
-            else:
-                print("   {}".format(text), file=buf)
+            print(self.line_formatter(i, line, lineno=lineno), file=buf)
 
         d["output"] = buf.getvalue()
         d.update(_get_info_from_exception(exc))
         return d
+
+
+## xxx: remove it
+def _get_info_from_exception(exc: jinja2.TemplateError):
+    d = {
+        "exc_class": "{}.{}".format(
+            getattr(exc, "__module__", "builtins"), exc.__class__.__name__
+        ),
+        "message": str(exc),
+    }
+
+    if hasattr(exc, "filename"):
+        d["where"] = exc.filename or exc.name
+        return d
+
+    # hack: original expression from hidden api
+    if hasattr(exc, "original_context"):
+        octx = exc.original_context
+        if octx.where is not None:
+            d["where"] = os.path.relpath(octx.where, os.getcwd())
+        d["message"] = d["message"].replace(exc.args[0], octx.path)
+    return d
 
 
 def get_gentle_output_from_exception(
