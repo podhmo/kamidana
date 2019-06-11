1bbbc8a0d244858948fd1ced0671ca71cbbef711
podhmo <ababjam61+github@gmail.com>
2019-06-12 08:51:22 +0900

    bump version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ testing_extras = tests_require + []
 
 setup(
     name="kamidana",
-    version="0.7.2",
+    version="0.8.0",
     description="command line jinja2 template (yet another j2cli)",
     long_description=README + "\n\n" + CHANGES,
     classifiers=[
