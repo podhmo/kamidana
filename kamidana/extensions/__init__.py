3bf0af6fae32365371842c7c1c17915a9af5da91
podhmo <ababjam61+github@gmail.com>
2019-06-12 08:09:27 +0900

    feat: support cookiecutter

--- kamidana/extensions/__init__.py
+++ kamidana/extensions/__init__.py
@@ -13,11 +13,11 @@ def _build_additionals(modules) -> dict:
     return additionals
 
 
-def create_apply_additonal_modules_extension_class(name: str, *, modules: list):
-    doc = "extension create from {}".format(", ".join(modules))
-
+def create_apply_additonal_modules_extension_class(name: str, *, doc, get_modules):
     def __init__(self, environment: Environment) -> None:
         super(cls, self).__init__(environment)
+
+        modules = get_modules(environment)
         additionals = _build_additionals(modules)
         for name, defs in additionals.items():
             getattr(environment, name).update(defs)
@@ -27,9 +27,41 @@ def create_apply_additonal_modules_extension_class(name: str, *, modules: list):
     return cls
 
 
-NamingHelperExtension = create_apply_additonal_modules_extension_class(
-    "NamingHelperExtension", modules=["kamidana.additionals.naming"]
+NamingModuleExtension = create_apply_additonal_modules_extension_class(
+    "NamingModuleExtension",
+    get_modules=lambda env: ["kamidana.additionals.naming"],
+    doc="extension create from kamidana.additionals.naming",
 )
-ReaderHelperExtension = create_apply_additonal_modules_extension_class(
-    "ReaderHelperExtension", modules=["kamidana.additionals.reader"]
+ReaderModuleExtension = create_apply_additonal_modules_extension_class(
+    "ReaderModuleExtension",
+    get_modules=lambda env: ["kamidana.additionals.reader"],
+    doc="extension create from kamidana.additionals.reader",
+)
+
+## for cookiecutter
+def _extract_module_from_cookiecutter_cotext(env, *, exception_cls=ImportError):
+    import inspect
+
+    # black magic (collect context arguments, from stackframes)
+    _context = None
+    f = inspect.currentframe()
+    while f.f_back:
+        if "context" in f.f_locals:
+            _context = f.f_locals["context"]
+            break
+        f = f.f_back
+    if _context is None:
+        raise exception_cls("cookiecutter's context is not found, something wrong?")
+    if "cookiecutter" not in _context:
+        raise exception_cls("'cookiecutter' is not found in context, something wrong??")
+
+    if "_additional_modules" not in _context["cookiecutter"]:
+        raise exception_cls("we needs '_additional_modules' in your cookiecutter.json")
+    return _context["cookiecutter"]["_additional_modules"]
+
+
+CookiecutterAdditionalModulesExtension = create_apply_additonal_modules_extension_class(
+    "CookiecutterAdditionalModulesExtension",
+    get_modules=_extract_module_from_cookiecutter_cotext,
+    doc="activate additional modules, see context['cookiecutter']['_additional_modules'], created from your cookiecutter.json",
 )
